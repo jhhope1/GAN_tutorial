0003737adc76ee5108f8859b8da57b9c4df5dbdd
jhhope1 <jhhope1@naver.com>
2018-11-08 17:31:53 +0900

    asdf

--- GAN_MNIST/CNN_GAN/GAN_mnist.py
+++ GAN_MNIST/CNN_GAN/GAN_mnist.py
@@ -6,14 +6,14 @@ import torch.nn as nn
 from torchvision import transforms
 from torchvision.utils import save_image
 
-
+CUDA_VISIBLE_DEVICES=0
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 latent_size = 100
-hidn1=500
-hidn2=500
+hidn1=100
+hidn2=100
 image_size = 784
 num_epochs = 200
 batch_size = 100
@@ -68,7 +68,8 @@ class Dnet(nn.Module):
             nn.Sigmoid()
             )
     def forward(self,input):
-        return torch.cat([self.hid1(input),self.hid2(input)],dim=1)
+        out=torch.cat([self.hid1(input),self.hid2(input)],dim=1)
+        return out
 
 class Gnet(nn.Module):
     def __init__(self):
@@ -99,9 +100,9 @@ D = D.to(device)
 G = G.to(device)
 
 # Binary cross entropy loss and optimizer
-criterion = nn.BCELoss()
+criterion = nn.BCELoss().cuda()
 d_optimizer = torch.optim.Adam(D.parameters(), lr=0.000002)
-g_optimizer = torch.optim.Adam(G.parameters(), lr=0.000002)
+g_optimizer = torch.optim.Adam(G.parameters(), lr=0.002)
 
 def denorm(x):
     out = (x + 1) / 2
@@ -109,25 +110,26 @@ def denorm(x):
 
 def reset_grad():
     d_optimizer.zero_grad()
-    g_optimizer.zero_grad()
+    #g_optimizer.zero_grad()
 
 # Start training
 total_step = len(data_loader)
 for epoch in range(num_epochs):
-    batch_zeros=torch.Tensor(batch_size,10)
+    
     for i, (images, targets) in enumerate(data_loader):
+        batch_zeros=torch.zeros(batch_size,10)
         images = images.reshape(batch_size, -1).to(device)
 
-        real_labels = (torch.ones(batch_size, 1)).to(device)
+        real_labels = torch.ones(batch_size, 1).to(device)
         fake_labels = torch.zeros(batch_size, 1).to(device)
 
-        onehot=batch_zeros.scatter_(1, targets.reshape(batch_size,1) ,1).to(device)
+        onehot=batch_zeros.scatter(1, targets.reshape(batch_size,1) ,1).to(device)
         outputs = D(images)
 
         real_labels=torch.cat([onehot,real_labels],dim=1)
         
         d_loss_real = criterion(outputs, real_labels)
-        real_score = outputs
+        real_score = outputs.index_select(1,torch.Tensor([10]).long().cuda())
         
         
         rdint=np.random.random_integers(0,9,(batch_size))
@@ -142,12 +144,14 @@ for epoch in range(num_epochs):
         fake_images = G(rd)
         outputs = D(fake_images)
         d_loss_fake = criterion(outputs, fake_labels)
-        fake_score = outputs
+        fake_score = outputs.index_select(1,torch.Tensor([10]).long().cuda())
         
         d_loss = d_loss_real + d_loss_fake
         reset_grad()
-        d_loss.backward()
-        d_optimizer.step()
+
+        if(fake_score.mean().item()>0.5):
+            d_loss.backward()
+            d_optimizer.step()
         
         z = torch.randn(batch_size, latent_size).to(device)
         z=torch.cat([id_onehot,z],dim=1)
@@ -157,10 +161,11 @@ for epoch in range(num_epochs):
         g_loss = criterion(outputs, real_labels)
         
         reset_grad()
+
         g_loss.backward()
         g_optimizer.step()
         
-        if (i+1) % 200 == 0:
+        if ((i+1) % 200 == 0):
             print('Epoch [{}/{}], Step [{}/{}], d_loss: {:.4f}, g_loss: {:.4f}, D(x): {:.2f}, D(G(z)): {:.2f}' 
                   .format(epoch, num_epochs, i+1, total_step, d_loss.item(), g_loss.item(), 
                           real_score.mean().item(), fake_score.mean().item()))
@@ -174,11 +179,10 @@ for epoch in range(num_epochs):
                 result = torch.index_select(result, 1, indices)
                 error = criterion(result,test_zeros.scatter_(1, targets_.reshape(-1,1) ,1).to(device))
                 ans = torch.sum(torch.eq(torch.argmax(result,dim=1),targets_.reshape(-1).to(device)))
-                print("classification accuracy = ",ans.item()," error = ",error.item())
+                print("classification accuracy = ",ans.item()/len(test_mnist)," error = ",error.item())
     if (epoch+1) == 1:
         images = images.reshape(images.size(0), 1, 28, 28)
         save_image(denorm(images), os.path.join(sample_dir, 'real_images.png'))
-    
     fake_images = fake_images.reshape(fake_images.size(0), 1, 28, 28)
     save_image(denorm(fake_images), os.path.join(sample_dir, 'fake_images-{}.png'.format(epoch+1)))
 
